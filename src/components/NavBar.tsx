3aa51a39edeff25e07cb3ec8855bd481f0a0c7f6
SaraMecinovic1 <saramecinovic87@gmail.com>
2024-09-23 15:44:30 +0200

    fixed problem

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,21 +1,19 @@
 import { useState } from "react";
-import { Bars3Icon,XMarkIcon } from "@heroicons/react/16/solid";
-import Logo from "@/assets/Logo.png"
-import { nav } from "framer-motion/client";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import Logo from "@/assets/Logo.png";
 
-type Props = {}
+type Props = {};
 
 const NavBar = (props: Props) => {
-const flexBetween="flex items-center justify-between";
+  const flexBetween = "flex items-center justify-between";
 
   return (
     <nav>
-    <div
-    className={‘${flexBetween} fixed top-0 z-30 w-full‘}
-    >
-
-    </div>
+      <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+        {/* Dodatni sadržaj može ići ovde */}
+      </div>
     </nav>
-  )
-}
-export default NavBar ;
+  );
+};
+
+export default NavBar; // Ovaj deo osigurava da se komponenta izveze pravilno
