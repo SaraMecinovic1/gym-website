217a6e48555663438f016217ccbfb3500d577649
SaraMecinovic1 <saramecinovic87@gmail.com>
2024-11-05 14:02:04 +0100

    made scroll nav color

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import NavBar from "./scenes/navbar";
 import { Page } from "./shared/types";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<Page>(Page.Home);
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(Page.Home);
+      }
+      if (window.scrollY !== 0) {
+        setIsTopOfPage(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
   return (
     <div className="app bg-gray-20">
-      <NavBar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+      <NavBar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
     </div>
   );
 }
