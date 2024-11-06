60af97fa053a5167ee1face610841a2c88e4ac5b
SaraMecinovic1 <saramecinovic87@gmail.com>
2024-11-06 14:23:23 +0100

    sponsors are done

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -8,6 +8,7 @@ import SponsorRedBull from "@/assets/SponsorRedBull.png";
 import SponsorForbes from "@/assets/SponsorForbes.png";
 import SponsorFortune from "@/assets/SponsorFortune.png";
 import AloYoga from "@/assets/AloYoga.png";
+import { motion } from "framer-motion";
 
 type Props = {
   setSelectedPage: (value: Page) => void;
@@ -19,11 +20,24 @@ function Home({ setSelectedPage }: Props) {
   return (
     <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
       {/* IMAGE AND MAIN HEADER */}
-      <div className="md:flex  mx-auto w-5/6  items-center justify-center md:h-5/6">
+      <motion.div
+        onViewportEnter={() => setSelectedPage(Page.Home)}
+        className="md:flex  mx-auto w-5/6  items-center justify-center md:h-5/6"
+      >
         {/* MAIN HEADER */}
         <div className="z-10 mt-32 md:basis-3/5">
           {/* HEADINGS */}
-          <div className="md:-mt-20">
+          <motion.div
+            className="md:-mt-20"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 0.8 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+            }}
+          >
             <div className="relative">
               <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] md:before:content-evolvetext">
                 <img src={HomePageText} alt="home-page-text" />
@@ -34,9 +48,20 @@ function Home({ setSelectedPage }: Props) {
               Studios to get the Body Shapes That you Dream of.. Get Your Dream
               Body Now.
             </p>
-          </div>
+          </motion.div>
+
           {/* ACTIONS */}
-          <div className="mt-8 flex items-center gap-8  ">
+          <motion.div
+            className="mt-8 flex items-center gap-8  "
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ delay: 0.2, duration: 0.8 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+            }}
+          >
             <button>
               <ActionButton setSelectedPage={setSelectedPage}>
                 Join Now
@@ -49,19 +74,23 @@ function Home({ setSelectedPage }: Props) {
             >
               <p>Learn More</p>
             </AnchorLink>
-          </div>
+          </motion.div>
         </div>
 
         {/* IMAGE */}
-        <div className="flex basis-3/5 justify-center md:z-10  md:ml-40 md:mt-16 md:justify-items-center">
-          <img src={HomePageGrafic} alt="home-pageGraphic" />
+        <div className="flex  basis-3/5 justify-center md:z-10  md:ml-40 md:mt-16 md:justify-items-end">
+          <img
+            className="w-[400px] max-w-full h-auto"
+            src={HomePageGrafic}
+            alt="home-pageGraphic"
+          />
         </div>
-      </div>
+      </motion.div>
 
       {isAboveMediumScreen && (
-        <div>
-          <div>
-            <div>
+        <div className="h-[150px]  w-full bg-primary-100 py-10">
+          <div className="mx-auto w-5/6  flex justify-center">
+            <div className=" flex  w-3/4 items-center justify-between gap-8">
               <img
                 className="w-[90px] h-[50px]"
                 src={AloYoga}
