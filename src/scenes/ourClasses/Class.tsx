65a7728cd284188bf68d3fd51e1909d2c75b98cd
SaraMecinovic1 <saramecinovic87@gmail.com>
2024-11-11 15:16:43 +0100

    made swipper

--- src/scenes/ourClasses/Class.tsx
+++ src/scenes/ourClasses/Class.tsx
@@ -5,14 +5,22 @@ type Props = {
 };
 
 const Class = ({ name, description, image }: Props) => {
-  const overlayStyles = `p-5 absolute z-30 flex h-[380px] w-[450px] flex-col items-center justify-center whitespace-normal bg-primary-500 text-center text-white opacity-0 transition duration-500 hover:opacity-90`;
+  const overlayStyles = `p-5 absolute z-30 flex
+      h-[380px] w-[400px] flex-col items-center justify-center
+      whitespace-normal bg-primary-500 text-center text-white
+      opacity-0 transition duration-500 hover:opacity-90`;
+
   return (
-    <li className="relative mx-5 inline-block h-[380px] w-[450px]">
+    <li className="relative w-[400px] h-[380px] flex-shrink-0">
       <div className={overlayStyles}>
-        <p>{name}</p>
-        <p>{description}</p>
-        <img src={image} alt="class" />
+        <p className="text-2xl">{name}</p>
+        {description && <p className="mt-5">{description}</p>}
       </div>
+      <img
+        className="w-full h-full object-cover border border-gray"
+        alt={name}
+        src={image}
+      />
     </li>
   );
 };
--- src/scenes/ourClasses/index.tsx
+++ src/scenes/ourClasses/index.tsx
@@ -1,12 +1,12 @@
-import { ClassType, Page } from "@/shared/types";
+import { Page, ClassType } from "@/shared/types";
 import image1 from "@/assets/image1.png";
 import image2 from "@/assets/image2.png";
 import image3 from "@/assets/image3.png";
 import image4 from "@/assets/image4.png";
 import image5 from "@/assets/image5.png";
 import image6 from "@/assets/image6.png";
-import HText from "@/shared/HText";
 import { motion } from "framer-motion";
+import HText from "@/shared/HText";
 import Class from "./Class";
 
 const classes: Array<ClassType> = [
@@ -48,10 +48,10 @@ type Props = {
   setSelectedPage: (value: Page) => void;
 };
 
-function OurClasses({ setSelectedPage }: Props) {
+const OurClasses = ({ setSelectedPage }: Props) => {
   return (
-    <section id="ourclasses" className="w-full bg-primary-100 py-40 ">
-      <motion.div onViewportEnter={() => setSelectedPage(Page)}>
+    <section id="ourclasses" className="w-full bg-primary-100 py-40">
+      <motion.div onViewportEnter={() => setSelectedPage(Page.OurClasses)}>
         <motion.div
           className="mx-auto w-5/6"
           initial="hidden"
@@ -63,8 +63,8 @@ function OurClasses({ setSelectedPage }: Props) {
             visible: { opacity: 1, x: 0 },
           }}
         >
-          <div>
-            <HText>Our Classes</HText>
+          <div className="md:w-3/5">
+            <HText>OUR CLASSES</HText>
             <p className="py-5">
               Fringilla a sed at suspendisse ut enim volutpat. Rhoncus vel est
               tellus quam porttitor. Mauris velit euismod elementum arcu neque
@@ -72,23 +72,24 @@ function OurClasses({ setSelectedPage }: Props) {
               enim mattis odio in risus nunc.
             </p>
           </div>
-
-          <div className="mt-10 h-[353px] w-full overflow-x-auto overflow-y-hidden">
-            <ul className="w-[28000px] whitespace-nowrap">
-              {classes.map((item: ClassType, index) => (
-                <Class
-                  key={`${item.name}-${index}`}
-                  name={item.name}
-                  description={item.description}
-                  image={item.image}
-                />
-              ))}
-            </ul>
-          </div>
         </motion.div>
+
+        {/* Container for scrolling images */}
+        <div className="mt-10 mx-10 overflow-x-auto">
+          <ul className="flex gap-5 w-[1800px]">
+            {classes.map((item: ClassType, index) => (
+              <Class
+                key={`${item.name}-${index}`}
+                name={item.name}
+                description={item.description}
+                image={item.image}
+              />
+            ))}
+          </ul>
+        </div>
       </motion.div>
     </section>
   );
-}
+};
 
 export default OurClasses;
