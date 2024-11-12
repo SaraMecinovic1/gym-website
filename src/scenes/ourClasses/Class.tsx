877ad959e7429924630f2c2dd85a619353983640
SaraMecinovic1 <saramecinovic87@gmail.com>
2024-11-12 14:43:13 +0100

    finished contact us page completely

--- src/scenes/ourClasses/Class.tsx
+++ src/scenes/ourClasses/Class.tsx
@@ -8,7 +8,7 @@ const Class = ({ name, description, image }: Props) => {
   const overlayStyles = `p-5 absolute z-30 flex
         h-full w-full flex-col items-center justify-center
         whitespace-normal bg-primary-500 text-center text-white
-        transition duration-500 opacity-0 group-hover:opacity-90`;
+        transition duration-500 opacity-0 hover:opacity-90`;
 
   return (
     <li className="relative w-[400px] h-[380px] flex-shrink-0 group">
